cb0da3184381756fc0c470e11c75f8a691331842
griffinfarrow <gmmfarrow@gmail.com>
2024-11-24 17:43:56 +0000

    adding in a function to calculate confidence intervals for tmle

--- src/tmle.py
+++ src/tmle.py
@@ -25,3 +25,34 @@ def targeting_step(dataset, propensity_scores, pred_outcomes,
         print(model_epsilon.summary())
 
     return model_epsilon
+
+
+
+def infl_curve(df, corrected_Q1, corrected_Q0, propensity_scores, 
+                treatment_label='A', outcome_label='Y'):
+
+    d1 = (df[treatment_label] * (df[outcome_label] - corrected_Q1)/propensity_scores) + \
+                corrected_Q1 - corrected_Q1.mean()
+
+    d0 = ((1 - df[treatment_label]) * (df[outcome_label] - corrected_Q0))/(1 - propensity_scores) + \
+                corrected_Q0 - corrected_Q0.mean()
+
+    infl_curve = d1 - d0
+
+    return infl_curve
+
+
+
+def conf_int(df, corrected_Q1, corrected_Q0, propensity_scores, 
+                treatment_label='A', outcome_label='Y'):
+
+    infl_curv_val = infl_curve(df, corrected_Q1, corrected_Q0, propensity_scores, treatment_label, outcome_label)
+
+    std_err = np.sqrt(np.var(infl_curve_val)/(df[outcome_label].count()))
+
+    tmle_val = corrected_Q1.mean() - corrected_Q0.mean()
+
+    upper_conf = tmle_val + 1.96*std_err 
+    lower_conf = tmle_val - 1.96*std_err 
+
+    return lower_conf, upper_conf
